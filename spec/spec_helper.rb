f9ccc989226eeed17a6998dbf23a04492ccd38de
liaozhuoran <liaozhuoran35@hotmail.com>
2013-10-18 10:04:50 +0800

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # tian jia Capybara DSL dao bang zhu wen jian
+  config.include Capybara::DSL
 end
